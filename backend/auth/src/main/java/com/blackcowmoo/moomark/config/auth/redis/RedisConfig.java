730504cfb9eb36a17109fc8016bf7afb31496a1e
GyeongSu Han <micalgenus@gmail.com>
2020-06-24 23:02:35 +0900

    Add docker configuration of backend/auth (#13)
    * initial Next
    
    Ref: https://velog.io/@jakeseo_me/%EB%8B%A4%EC%8B%9C-%ED%94%84%EB%A1%A0%ED%8A%B8-%ED%8B%80%EC%9E%A1%EA%B8%B0-1-%ED%83%80%EC%9E%85%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8-next.js-9
    
    * fix lint excute $yarn eslint --fix
    
    * Update github actions for typescript
    
    * fix yarn build & yarn test
    
    * Update docker configuration
    
    * Create docker configuration for backend/auth
    
    * Update docker with redis
    
    * Update redis config
    
    * Add dockerignore in gateway
    
    * Revert yarn.lock in frontend
    
    * Update deploy script for travis
    
    * Update actions
    
    * Update package for docker
    
    * Fix actions
    
    Co-authored-by: Park Jein <55627999+pjainxido@users.noreply.github.com>
    
    Fix package
    
    Fix package

--- backend/auth/src/main/java/com/blackcowmoo/moomark/config/auth/redis/RedisConfig.java
+++ backend/auth/src/main/java/com/blackcowmoo/moomark/config/auth/redis/RedisConfig.java
@@ -1,9 +1,6 @@
 package com.blackcowmoo.moomark.config.auth.redis;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.blackcowmoo.moomark.model.User;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,28 +8,39 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableRedisRepositories
 public class RedisConfig {
+  @Value("${spring.redis.host}")
+  private String host;
+
+  @Value("${spring.redis.port}")
+  private int port;
 
   @Value("${spring.redis.password}")
-  private String redisPwd;
-
-  @Bean
-  public ObjectMapper objectMapper() {
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-    mapper.registerModules(new JavaTimeModule(), new Jdk8Module());
-    return mapper;
-  }
+  private String password;
 
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
     RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-    redisStandaloneConfiguration.setPassword(redisPwd);
+    redisStandaloneConfiguration.setHostName(host);
+    redisStandaloneConfiguration.setPort(port);
+    redisStandaloneConfiguration.setPassword(password); // redis에 비밀번호가 설정 되어 있는 경우 설정해주면 됩니다.
     LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
     return lettuceConnectionFactory;
+  }
+
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate() {
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    // 객체를 json 형태로 깨지지 않고 받기 위한 직렬화 작업
+    redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(User.class));
+
+    return redisTemplate;
   }
 }
