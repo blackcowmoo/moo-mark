10553ab1ccb14a9bb199ab54ed776519a697482a
IM CHAECHEOL <dlacocjf32@gmail.com>
2020-07-07 02:54:01 +0900

    패키지명 {groupName}.moomark -> {groupName}.moomark.{serviceName} 변경 (#19)

---
+++ backend/auth/src/main/java/com/blackcowmoo/moomark/auth/service/UserService.java
@@ -0,0 +1,32 @@
+package com.blackcowmoo.moomark.auth.service;
+
+import com.blackcowmoo.moomark.auth.model.entity.User;
+import com.blackcowmoo.moomark.auth.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Service
+public class UserService {
+  @Autowired
+  UserRepository userRepository;
+
+  public User getUserById(long userId) {
+    return userRepository.getOne(userId);
+  }
+
+  public boolean updateUserNickname(long userId, String nickname) {
+    try {
+      User willUpdateUser = userRepository.getOne(userId);
+      willUpdateUser.updateNickname(nickname);
+      userRepository.save(willUpdateUser);
+    } catch (Exception e) {
+      log.error(e);
+      return false;
+    }
+    return true;
+  }
+}
