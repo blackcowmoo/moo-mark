15bb5668895cdd8e75552e381bbf0b4f46ed52bb
WonsangKi <dev.wonsang@gmail.com>
2020-07-08 03:04:40 +0900

    init 'feature/chat'
    and add build.gradle

---
+++ backend/chat/src/test/java/com/blackcowmoo/moomark/chat/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackcowmoo.moomark.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
