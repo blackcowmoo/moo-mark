07bbcd1da0fa2c8596f3c61d4c091128938f1af1
Apisapple <38449976+Apisapple@users.noreply.github.com>
2020-07-28 01:45:04 +0900

    update crawling.py
    pylint 오류 해결

--- backend/crawling/crawling.py
+++ backend/crawling/crawling.py
@@ -1,11 +1,37 @@
-from urllib.request import urlopen
+'''
+    2020. 07. 28
+'''
+
 from bs4 import BeautifulSoup
 import requests
 
 
-def crawling(maxPage):
+class WebInfo:
+    '''
+    Web 페이지 정보를 가지고 있습니다.
+    '''
+    def __init__(self):
+        self.__url = ""
+
+    def get_url(self):
+        '''
+        URL 반환
+        '''
+        return self.__url
+
+    def set_url(self, urlvalue):
+        '''
+        URL 세팅
+        '''
+        self.__url = urlvalue
+
+
+def crawling(maxpage):
+    '''
+    crawling 테스트 함수
+    '''
     page = 1
-    while page < maxPage:
+    while page < maxpage:
         url = 'http://creativeworks.tistory.com/' + str(page)
         source_code = requests.get(url)
         plain_text = source_code.text
