850744e423eb4d50c7c500e3b5bffb0536a7340d
Apisapple <38449976+Apisapple@users.noreply.github.com>
2020-07-30 00:23:01 +0900

    add setup.py

---
+++ backend/crawling/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name="moo_mark crawling",
+    version="1.0",
+    author="MinYongKim",
+    install_requires=[
+        'appdirs==1.4.4',
+        'astroid==2.4.2',
+        'beautifulsoup4==4.9.1',
+        'certifi==2020.6.20',
+        'chardet==3.0.4',
+        'colorama==0.4.3',
+        'distlib==0.3.1',
+        'filelock==3.0.12',
+        'idna==2.10',
+        'isort==4.3.21',
+        'lazy-object-proxy==1.4.3',
+        'lxml==4.5.2',
+        'mccabe==0.6.1',
+        'pycodestyle==2.6.0',
+        'pylint==2.5.3',
+        'requests==2.24.0',
+        'six==1.15.0',
+        'soupsieve==2.0.1',
+        'toml==0.10.1',
+        'urllib3==1.25.10',
+        'virtualenv==20.0.28',
+        'wrapt==1.12.1'
+    ]
+)
