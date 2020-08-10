7fc007d03f39e108b8baceff360dfff920b95e3d
GyeongSu Han <micalgenus@gmail.com>
2020-08-10 22:38:09 +0900

    Running development mode of frontend in PR (#71)
    * Add template page
    
    * development mode test
    
    * Add development mode
    
    * Delete unused file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  publicRuntimeConfig: { GRAPHQL_SERVER: process.env.GRAPHQL_SERVER, GOOGLE_OAUTH_CLIENT_ID: process.env.GOOGLE_OAUTH_CLIENT_ID },
+  publicRuntimeConfig: {
+    DEVELOPMENT_MODE: process.env.DEVELOPMENT_MODE || false,
+    GRAPHQL_SERVER: process.env.GRAPHQL_SERVER,
+    GOOGLE_OAUTH_CLIENT_ID: process.env.GOOGLE_OAUTH_CLIENT_ID,
+  },
   webpack: (config, { webpack }) => {
     config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
     return config;
