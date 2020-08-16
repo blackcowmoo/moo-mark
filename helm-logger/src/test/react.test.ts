d3644427efa51998ec4b95a667f610b050143e5c
micalgenus <micalgenus@gmail.com>
2020-08-17 01:05:40 +0900

    Fix lint

--- helm-logger/src/test/react.test.ts
+++ helm-logger/src/test/react.test.ts
@@ -5,5 +5,6 @@ describe.skip('React DOM', () => {
   it('DOM to HTLM', async () => {
     const time = moment().toISOString();
     const html = renderLogs([{ timestamp: time, text: time }]);
+    console.log(html);
   });
 });
