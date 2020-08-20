4248f34575b9dd1f753829ec72ccceac899a840d
Apisapple <38449976+Apisapple@users.noreply.github.com>
2020-08-21 00:48:34 +0900

    Update navernews.py
    뉴스 정보 기사 제목, 신문사 및
    페이지별 정보 파싱

--- backend/crawling/navernews.py
+++ backend/crawling/navernews.py
@@ -1,5 +1,6 @@
 '''''
     2020. 08. 04
+    @author kmy
 '''''
 
 from bs4 import BeautifulSoup
@@ -8,16 +9,24 @@ from web_info import WebInfo
 
 def crawling(search_text):
     '''
-    crawling 테스트 함수
+    naver news crawling
     '''
-    naver_info = WebInfo()
-    url = 'https://search.naver.com/search.naver?where=news&sm=tab_jum&query='
-    full_url = url + str(search_text)
-    naver_info.set_url(full_url)
-    source_code = requests.get(naver_info.get_url(), headers={'User-Agent':'Mozilla/5.0'})
-    plain_text = source_code.text
-    html = BeautifulSoup(plain_text, 'lxml')
-    print(html)
 
+    naver_info = WebInfo()
+    for i in range(1, 100, 10):
+        url = 'https://search.naver.com/search.naver?where=news&sm=tab_jum&query='
+        full_url = url + str(search_text)
+        full_url = full_url + "start="
+        full_url = full_url + str(i)
+        naver_info.set_url(full_url)
+        source_code = requests.get(naver_info.get_url(), headers={'User-Agent':'Mozilla/5.0'})
+        plain_text = source_code.text
+        html = BeautifulSoup(plain_text, 'lxml')
+        articles = html.select("ul.type01 > li")
+
+        for article in articles:
+            title = article.select_one("a._sp_each_title").text
+            source = article.select_one("span._sp_each_source").text
+            print(title, source)
 
 crawling("정보보안")
