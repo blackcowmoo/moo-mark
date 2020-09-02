f40f4392972c8bba1bf939f9a7684c1389309b43
Park Jein <55627999+pjainxido@users.noreply.github.com>
2020-09-03 02:08:27 +0900

    Add test page and mockdata

--- helm-logger/src/core/app.ts
+++ helm-logger/src/core/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { getLog, getPod } from './kubectl';
 import { renderLogs } from './page';
+import MockData from '../test/data/mock.data';
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,10 @@ app.use(express.json());
 // For kubernetes
 app.get('/healthz', (req, res) => {
   res.send('OK');
+});
+
+app.get('/test', (req, res) => {
+  res.send(renderLogs(MockData));
 });
 
 app.get('/:pod', async (req, res) => {
